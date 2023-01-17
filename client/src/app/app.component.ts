55c88e05663663d16dd4d7f2858768139a07fb9c
EllieShilla <74963526+EllieShilla@users.noreply.github.com>
2023-01-17 16:35:21 +0200

    Create Client Part

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  users: any;
+  title = 'Dating App';
+  baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+     this.http.get(this.baseUrl + 'users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has completed'),
+    });
+  }
+}
