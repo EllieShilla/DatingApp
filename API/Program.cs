b012bdcf3ed874e73cedf7b15bd1471eed61274a
EllieShilla <74963526+EllieShilla@users.noreply.github.com>
2024-06-10 14:57:49 +0300

    Authentication basics

--- API/Program.cs
+++ API/Program.cs
@@ -1,24 +1,32 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt=>
-{
-opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationService(builder.Configuration);
+builder.Services.AddIdentityService(builder.Configuration);
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder=>builder.AllowAnyHeader().
+app.UseCors(builder => builder.AllowAnyHeader().
                              AllowAnyMethod().
                              WithOrigins("https://localhost:4200"));
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
- 
+
 app.Run();
