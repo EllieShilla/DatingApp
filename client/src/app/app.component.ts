fd973be0dab97ddafcef3a8fe1fb43a72d30a72a
EllieShilla <74963526+EllieShilla@users.noreply.github.com>
2024-06-15 16:16:34 +0300

    Added routing to client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AccountService } from './_services/account.service';
 import { User } from './_models/user';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent implements OnInit {
 
   constructor(
     private http: HttpClient,
-    private accountService: AccountService
+    private accountService: AccountService,
+    private toastr: ToastrService
   ) {}
 
   ngOnInit(): void {
@@ -24,7 +26,7 @@ export class AppComponent implements OnInit {
   getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: (response) => (this.users = response),
-      error: (error) => console.log(error),
+      error: (error) => this.toastr.error(error.error),
       complete: () => {},
     });
   }
